05c69c2c8eb0cfb98bda744da6485ca7f834314c
name <example@example.com>
2023-05-04 18:27:26 +0200

    Removed duplicate spike_once->degr rec. synapse creation.

--- src/snnadaptation/population/apply_population_coding.py
+++ src/snnadaptation/population/apply_population_coding.py
@@ -1,11 +1,15 @@
 """Applies population coding to an incoming algorithm."""
 import copy
-from typing import Dict, List, Tuple
+from typing import Dict
 
 import networkx as nx
-from snnbackends.networkx.LIF_neuron import Identifier, LIF_neuron, Synapse
+from snnbackends.networkx.LIF_neuron import LIF_neuron
 from snncompare.export_plots.Plot_config import Plot_config
 from typeguard import typechecked
+
+from snnadaptation.population.create_population_neurons import (
+    get_population_neuron_properties,
+)
 
 
 @typechecked
@@ -39,46 +43,10 @@ def apply_population_coding(
         )
         for red_level in range(1, redundancy + 1):
             # Create redundant neurons.
-            create_redundant_node(
+            create_redundant_population_node(
                 adaptation_graph=adaptation_graph,
                 node_name=node_name,
                 plot_config=plot_config,
-                red_level=red_level,
-            )
-
-    for red_level in range(1, redundancy + 1):
-        # Start new loop before adding edges, because all redundant neurons
-        # need to exist before creating synapses.
-        for node_name in original_nodes:
-            # Add input synapses to redundant node.
-            add_input_synapses(
-                adaptation_graph=adaptation_graph,
-                node_name=node_name,
-                red_level=red_level,
-            )
-
-            # Add output synapses to redundant node.
-            add_output_synapses(
-                adaptation_graph=adaptation_graph,
-                node_name=node_name,
-                red_level=red_level,
-            )
-            add_inhibitory_outgoing_synapses(
-                adaptation_graph=adaptation_graph,
-                node_name=node_name,
-                max_red_level=redundancy,
-            )
-
-            # Add inhibitory synapse from node to redundant node.
-            # add_inhibitory_synapse(
-            #    adaptation_graph=adaptation_graph,
-            #    node_name=node_name,
-            #    red_level=red_level,
-            # )
-
-            add_recurrent_inhibitiory_synapses(
-                adaptation_graph=adaptation_graph,
-                node_name=node_name,
                 red_level=red_level,
             )
 
@@ -118,7 +86,7 @@ def store_output_synapses(
 
 
 @typechecked
-def create_redundant_node(
+def create_redundant_population_node(
     *,
     adaptation_graph: nx.DiGraph,
     node_name: str,
@@ -133,14 +101,13 @@ def create_redundant_node(
     :param node_name: Node of the name of a networkx graph.
     """
 
-    # TODO: include spike={}, is_redundant=True,
     ori_lif = adaptation_graph.nodes[node_name]["nx_lif"][0]
     bare_node_name = ori_lif.name
     identifiers = ori_lif.identifiers
 
     redundant_neuron_properties: Dict[
         str, float
-    ] = computer_red_neuron_properties(
+    ] = get_population_neuron_properties(
         adaptation_graph=adaptation_graph,
         node_name=node_name,
         red_level=red_level,
@@ -170,233 +137,3 @@ def create_redundant_node(
 
     adaptation_graph.add_node(lif_neuron.full_name)
     adaptation_graph.nodes[lif_neuron.full_name]["nx_lif"] = [lif_neuron]
-
-
-@typechecked
-def computer_red_neuron_properties(
-    *, adaptation_graph: nx.DiGraph, node_name: str, red_level: int
-) -> Dict[str, float]:
-    """Computes the redundant neuron properties such that they take over in the
-    right settings."""
-    if node_name[:9] != "selector_" and node_name[:11] != "next_round_":
-        # if node_name[:9] != "selector_":
-        bias = adaptation_graph.nodes[node_name]["nx_lif"][0].bias.get()
-        du = adaptation_graph.nodes[node_name]["nx_lif"][0].du.get()
-        dv = adaptation_graph.nodes[node_name]["nx_lif"][0].dv.get()
-        vth = compute_vth_for_delay(  # Different vals for different neurons.
-            adaptation_graph=adaptation_graph,
-            node_name=node_name,
-            red_level=red_level,
-        )
-    elif node_name[:11] == "next_round_":
-        bias = adaptation_graph.nodes[node_name]["nx_lif"][0].bias.get()
-        du = adaptation_graph.nodes[node_name]["nx_lif"][0].du.get()
-        dv = adaptation_graph.nodes[node_name]["nx_lif"][0].dv.get()
-        vth = adaptation_graph.nodes[node_name]["nx_lif"][0].vth.get()
-    else:
-        m_val_identifier: Identifier = adaptation_graph.nodes[node_name][
-            "nx_lif"
-        ][0].identifiers[1]
-        if m_val_identifier.description == "m_val":
-            if m_val_identifier.value == 0:
-                bias = 1.0
-                du = 0.1
-                dv = 0.0
-                vth = float(red_level)
-            else:
-                # Designed using neuron discovery grid search. Limited to a
-                # redundancy of max 4, because after that adding +1 to vth
-                # does not result in the selector neuron spiking 1 timestep
-                # later (w.r.t. an incoming spike at fixed arbitrary time t).
-                bias = 0.0
-                du = 0.1
-                dv = 0.0
-                vth = float(red_level)  # Add delay in when redundant redundant
-                # etc. neurons take over.
-        else:
-            raise ValueError(
-                "Error, node identifier was not m_val for selector node."
-            )
-
-    return {
-        "bias": bias,
-        "du": du,
-        "dv": dv,
-        "vth": vth,
-    }
-
-
-@typechecked
-def compute_vth_for_delay(
-    *, adaptation_graph: nx.DiGraph, node_name: str, red_level: int
-) -> float:
-    """Increases vth with 1 to realise a delay of t=1 for the redundant
-    spike_once neurons, rand neurons and selector neurons.
-
-    Returns dv of default node otherwise.
-
-    :param adaptation_graph: Graph with the MDSA SNN approximation solution.
-    :param node_name: Node of the name of a networkx graph.
-    """
-    if node_name[:11] == "next_round_":
-        vth = adaptation_graph.nodes[node_name]["nx_lif"][0].vth.get() + 1
-    elif (
-        node_name[:16] == "degree_receiver_"
-        or node_name[:11] == "spike_once_"
-        or node_name[:5] == "rand_"
-    ):
-        vth = (
-            adaptation_graph.nodes[node_name]["nx_lif"][0].vth.get()
-            + red_level
-        )
-    else:
-        vth = adaptation_graph.nodes[node_name]["nx_lif"][0].vth.get()
-    return vth
-
-
-@typechecked
-def add_input_synapses(
-    *, adaptation_graph: nx.DiGraph, node_name: str, red_level: int
-) -> None:
-    """
-
-    :param adaptation_graph: Graph with the MDSA SNN approximation solution.
-    :param node_name: Node of the name of a networkx graph.
-
-    """
-    for edge in adaptation_graph.nodes[node_name]["input_edges"]:
-        # Compute set edge weight
-        left_node_name = edge[0]
-        right_node_name = f"r_{red_level}_{node_name}"
-
-        if node_name[:9] == "selector_" and edge[0][:11] == "next_round_":
-            # The redundant selector neurons only start firing n seconds after
-            # the next_round neuron has fired.
-            weight = 1
-        else:
-            weight = adaptation_graph[edge[0]][edge[1]]["synapse"].weight
-
-        edges: List[Tuple[str, str]] = [(left_node_name, right_node_name)]
-        if left_node_name[:11] == "next_round_":
-            # print(f'add:{(left_node_name, right_node_name)}')
-            edges.append((f"r_{red_level}_{left_node_name}", right_node_name))
-
-        # Create edge
-        adaptation_graph.add_edges_from(
-            edges,
-            synapse=Synapse(
-                weight=weight,
-                delay=0,
-                change_per_t=0,
-            ),
-            is_redundant=True,
-        )
-    # if node_name == "selector_0_1":
-    # exit()
-
-
-@typechecked
-def add_output_synapses(
-    *, adaptation_graph: nx.DiGraph, node_name: str, red_level: int
-) -> None:
-    """
-
-    :param adaptation_graph: Graph with the MDSA SNN approximation solution.
-    :param node_name: Node of the name of a networkx graph.
-
-    """
-    adaptation_graph.add_edges_from(
-        adaptation_graph.nodes[node_name]["output_edges"]
-    )
-    for edge in adaptation_graph.nodes[node_name]["output_edges"]:
-        # Compute set edge weight
-        left_node_name = f"r_{red_level}_{node_name}"
-        right_node_name = edge[1]
-        weight = adaptation_graph[edge[0]][edge[1]]["synapse"].weight
-
-        adaptation_graph.add_edges_from(
-            [(left_node_name, right_node_name)],
-            synapse=Synapse(
-                weight=weight,
-                delay=0,
-                change_per_t=0,
-            ),
-            is_redundant=True,
-        )
-
-
-@typechecked
-def add_inhibitory_outgoing_synapses(
-    *, adaptation_graph: nx.DiGraph, node_name: str, max_red_level: int
-) -> None:
-    """Adds inhibitory synapse for selector neuron."""
-
-    edges = []
-    # Add edge from selector into redundant selectors
-    for red_level in range(1, max_red_level + 1):
-        if "counter" not in node_name:
-            edges.append((node_name, f"r_{red_level}_{node_name}"))
-
-        # Add edge from redundant selector to remaining redundant selectors
-        for right_red_level in range(red_level + 1, max_red_level + 1):
-            # if "counter" in node_name:
-            # print(f'recurrent counter edge!')
-            # exit()
-            if "counter" not in node_name:
-                edges.append(
-                    (
-                        f"r_{red_level}_{node_name}",
-                        f"r_{right_red_level}_{node_name}",
-                    )
-                )
-    adaptation_graph.add_edges_from(
-        edges,
-        synapse=Synapse(
-            weight=-100,
-            delay=0,
-            change_per_t=0,
-        ),
-        is_redundant=True,
-    )
-
-
-@typechecked
-def add_recurrent_inhibitiory_synapses(
-    *, adaptation_graph: nx.DiGraph, node_name: str, red_level: int
-) -> None:
-    """
-
-    :param adaptation_graph: Graph with the MDSA SNN approximation solution.
-    :param node_name: Node of the name of a networkx graph.
-
-    """
-
-    if "recur" in adaptation_graph.nodes[node_name].keys():
-        if "counter" not in node_name:
-            adaptation_graph.add_edges_from(
-                [
-                    (
-                        f"r_{red_level}_{node_name}",
-                        f"r_{red_level}_{node_name}",
-                    )
-                ],
-                synapse=Synapse(
-                    weight=adaptation_graph.nodes[node_name]["recur"],
-                    delay=0,
-                    change_per_t=0,
-                ),
-            )
-    if node_name[:9] == "selector_":
-        adaptation_graph.add_edges_from(
-            [
-                (
-                    f"r_{red_level}_{node_name}",
-                    f"r_{red_level}_{node_name}",
-                )
-            ],
-            synapse=Synapse(
-                weight=4,
-                delay=0,
-                change_per_t=0,
-            ),
-        )
---
+++ src/snnadaptation/population/create_population_neurons.py
@@ -0,0 +1,129 @@
+"""Creates the different redundant population neurons, per neruon type.
+
+TODO: check multiplies with 0, e.g. vth*red_level with vth =0.
+"""
+from typing import Dict, List
+
+import networkx as nx
+from typeguard import typechecked
+
+
+@typechecked
+def get_population_neuron_properties(
+    *, adaptation_graph: nx.DiGraph, node_name: str, red_level: int
+) -> Dict[str, float]:
+    """Returns the neuron properties for population coding, per neuron type."""
+
+    unchanged_neuron_identifiers: List[str] = [
+        "spike_once",
+        "rand",
+        "degree_receiver",
+    ]
+    red_neuron_props: Dict[str, float] = {}
+    get_unchanged_neuron_properties(
+        adaptation_graph=adaptation_graph,
+        node_name=node_name,
+        red_neuron_props=red_neuron_props,
+    )
+    if not any(
+        unchanged_node_name in node_name
+        for unchanged_node_name in unchanged_neuron_identifiers
+    ):
+        return red_neuron_props
+    if "selector" in node_name:
+        get_population_selector_neuron_properties(
+            red_level=red_level,
+            red_neuron_props=red_neuron_props,
+        )
+    elif "counter" in node_name:
+        get_population_counter_neuron_properties(
+            red_level=red_level,
+            red_neuron_props=red_neuron_props,
+        )
+    elif "next_round" in node_name:
+        get_population_next_round_neuron_properties(
+            red_level=red_level,
+            red_neuron_props=red_neuron_props,
+        )
+    elif "terminator" in node_name:
+        get_population_terminator_neuron_properties(
+            red_level=red_level,
+            red_neuron_props=red_neuron_props,
+        )
+    else:
+        raise ValueError(f"Error, {node_name} not supported.")
+    return red_neuron_props
+
+
+@typechecked
+def get_unchanged_neuron_properties(
+    *,
+    adaptation_graph: nx.DiGraph,
+    node_name: str,
+    red_neuron_props: Dict[str, float],
+) -> None:
+    """These neuron types are unchanged for the MDSA algorithm."""
+    red_neuron_props["bias"] = adaptation_graph.nodes[node_name]["nx_lif"][
+        0
+    ].bias.get()
+    red_neuron_props["du"] = adaptation_graph.nodes[node_name]["nx_lif"][
+        0
+    ].du.get()
+    red_neuron_props["dv"] = adaptation_graph.nodes[node_name]["nx_lif"][
+        0
+    ].dv.get()
+    red_neuron_props["vth"] = adaptation_graph.nodes[node_name]["nx_lif"][
+        0
+    ].vth.get()
+
+
+@typechecked
+def get_population_selector_neuron_properties(
+    *,
+    red_level: int,
+    red_neuron_props: Dict[str, float],
+) -> None:
+    """The selector neuron needs a bias and vth multiplication with red_level.
+
+    The selector neurons for m=0 should fire at t=0. The selector
+    neurons for m=1 should fire when the next round neuron(s) have
+    fired.
+    """
+    red_neuron_props["bias"] = red_neuron_props["bias"] * red_level
+    red_neuron_props["vth"] = red_neuron_props["vth"] * red_level
+
+
+@typechecked
+def get_population_counter_neuron_properties(
+    *,
+    red_level: int,
+    red_neuron_props: Dict[str, float],
+) -> None:
+    """The counter_y_neurons need a vth of red_level-1 because they should
+    spike as soon as the accompanying  (population of) degree_receiver_x_y_z
+    neuron(s) has fired."""
+    red_neuron_props["vth"] = red_level - 1
+
+
+@typechecked
+def get_population_next_round_neuron_properties(
+    *,
+    red_level: int,
+    red_neuron_props: Dict[str, float],
+) -> None:
+    """The next_round neurons need a vth multiplication with red_level because
+    they should spike as soon as 1 degree_receiver neuron per node-circuit has
+    fired."""
+    red_neuron_props["vth"] = red_neuron_props["vth"] * red_level
+
+
+@typechecked
+def get_population_terminator_neuron_properties(
+    *,
+    red_level: int,
+    red_neuron_props: Dict[str, float],
+) -> None:
+    """The counter_y_neurons need a vth multiplication with red_level because
+    they should spike as soon as 1 degree_receiver neuron per node-circuit has
+    fired."""
+    red_neuron_props["vth"] = red_neuron_props["vth"] * red_level
